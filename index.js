679e26617cf8585626a8a82c7f391acb49a71da7
Meet Jivani <meet@Meets-MacBook-Air.local>
2023-02-04 17:40:12 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let card = document.getElementsByClassName("card");
+card.addEventListener("click",()=>{
+    document.getElementById("outer").innerHTML='<object type="type/html" data="home.html" ></object>';
+})
